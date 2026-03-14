a201dabbaebf87a1651048f735a628f3ea31af4d
Timothé <timothe.velasco@orange.fr>
2026-03-14 22:03:14 +0100

    init ci

---
+++ check_ci.sh
@@ -0,0 +1,17 @@
+check_rebase()
+{
+    hash_main=$(git show-ref -s origin/main)
+    hash_common=$(git merge-base origin/main ${{ github.sha }})
+
+    if [ "${hash_main}" = "${hash_common}" ]
+    then
+        echo "check_rebase: SUCCESS"
+        return 0
+    else
+        echo "check_rebase: FAILED"
+        success=0
+        return 1
+    fi
+}
+
+check_rebase
