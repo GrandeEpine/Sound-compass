153fd932e695fa7dbe08e64f58f41af2240440aa
Timothé <timothe.velasco@orange.fr>
2026-03-14 19:35:51 +0100

    First commit - creation of the project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Sound-compass
 New version of my Spotify web app using the Angular and Tailwind framework.
+
+## Features
+- Search for songs, albums, artists, and playlists.
+- Create and manage playlists.
+
+## Technologies Used
+- Angular: a JavaScript framework for building web applications. (version 21.2.2)
+- Tailwind CSS: a utility-first CSS framework for styling the application (version 4.2.1).
+- Spotify Web API: to interact with Spotify's music data and user accounts.
---
+++ sound-compass/src/app/app.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+import {Home} from './features/home/home';
+import {Header} from './layout/header/header';
+import {Footer} from './layout/footer/footer';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [Home, Header, Footer],
+  template: `
+    <app-header />
+    <main>
+      <section class="content">
+         <app-home />
+      </section>
+    </main>
+    <app-footer />
+  `,
+  styleUrl: './features/home/home.css'
+})
+export class App {
+  protected readonly title = signal('sound-compass');
+}
