f7cdb6db03d6e5420344365e4de1aa6b7e3fb949
Timothé <timothe.velasco@orange.fr>
2026-08-25 19:11:53 +0200

    Fix\ match the color and the state

--- sound-compass/src/app/features/playlist-creator/3-playlist-customize/playlist-customize.ts
+++ sound-compass/src/app/features/playlist-creator/3-playlist-customize/playlist-customize.ts
@@ -12,7 +12,7 @@ export class PlaylistCustomize {
   private APP_DESCRIPTION = "Made with Sound Compass";
   protected playlistName = signal<string | null>("");
   protected playlistDescription = signal<string | null>("");
-  protected isPublic = signal<boolean>(true);
+  protected isPublic = signal<boolean>(false);
   protected next = output<PlaylistCustomization>();
 
   confirm() {
@@ -22,5 +22,9 @@ export class PlaylistCustomize {
     }
     const description: string = this.APP_DESCRIPTION + this.playlistDescription();
     this.next.emit({name: this.playlistName() ?? "", description: description, isPublic: this.isPublic()});
+  }
+
+  protected setPrivacy(isPublic: boolean): void {
+    this.isPublic.set(isPublic);
   }
 }
